8afb0acdb3f7bb85fc3fb53e09d0db2725e4139f
Guralraz <87212802+Guralraz@users.noreply.github.com>
2021-12-09 22:48:25 +0100

    6.2 Added calculator class and unit tests in TestingMain class

--- Kodilla-Testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ Kodilla-Testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int additionResult = calculator.addAToB(5, 5);
+        int subtractionResult = calculator.subtractAFromB(5,5);
+
+        if (additionResult == 10) {
+            System.out.println("addAToB method is working");
+        } else {
+            System.out.println("addAToB method is not working");
+        }
+
+        if (subtractionResult == 0) {
+            System.out.println("subtractAFromB method is working");
+        } else {
+            System.out.println("subtractAFromB method is not working");
+        }
+
     }
+
 }
