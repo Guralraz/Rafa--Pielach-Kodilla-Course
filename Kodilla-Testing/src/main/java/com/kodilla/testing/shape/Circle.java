07732e801ac0f4190b052639834fa5a7fbc27260
Guralraz <87212802+Guralraz@users.noreply.github.com>
2021-12-12 05:02:45 +0100

    6.4 Added Shape interface, Square, Circle and Triangle Shape-implementing classes, ShapeCollector class and ShapeCollectorTestSuite unit test class

---
+++ Kodilla-Testing/src/main/java/com/kodilla/testing/shape/Circle.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.shape;
+
+public class Circle implements Shape {
+
+    public String getShapeName() {
+        return "Circle";
+    }
+
+    public String getField() {
+        return "\tU+03C0" + "r^2";
+    }
+
+}
---
+++ Kodilla-Testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,104 @@
+package com.kodilla.testing.shape;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Shape Collector Test Suite")
+public class ShapeCollectorTestSuite {
+
+    @DisplayName("Add/Remove method tests")
+
+    @Nested
+    class AddRemoveTests {
+
+        @Test
+        void testAddFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Circle circle = new Circle();
+
+            //When
+            shapeCollector.addFigure(circle);
+
+            //Then
+            assertEquals(1, shapeCollector.shapes.size());
+        }
+
+        @Test
+        void testRemoveNonExistentFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Triangle triangle = new Triangle();
+
+            //When
+            boolean result = shapeCollector.removeFigure(triangle);
+
+            //Then
+            assertFalse(result);
+        }
+
+        @Test
+        void testRemoveFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Square square = new Square();
+            shapeCollector.addFigure(square);
+
+            //When
+            boolean result = shapeCollector.removeFigure(square);
+
+            //Then
+            assertTrue(result);
+            assertEquals(0, shapeCollector.shapes.size());
+        }
+
+    }
+
+    @DisplayName("GetFigure/ShowFigures method tests")
+
+    @Nested
+    class GetFigureShowFiguresTests {
+
+        @Test
+        void testGetFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Circle circle = new Circle();
+            shapeCollector.addFigure(circle);
+
+            //When
+            Shape retrievedShape = shapeCollector.getFigure(0);
+
+            //Then
+            assertEquals(circle, retrievedShape);
+        }
+
+        @Test
+        void testShowFigures() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Circle circle = new Circle();
+            Triangle triangle = new Triangle();
+            Square square = new Square();
+            shapeCollector.addFigure(circle);
+            shapeCollector.addFigure(triangle);
+            shapeCollector.addFigure(square);
+
+            //When
+            List<Shape> testShapes = new ArrayList<>();
+            testShapes.add(circle);
+            testShapes.add(triangle);
+            testShapes.add(square);
+
+            //Then
+            assertEquals(testShapes, shapeCollector.showFigures());
+        }
+
+    }
+
+}
