f64d5deb712de7d7a17b1702184f84eb51e02aab
Guralraz <87212802+Guralraz@users.noreply.github.com>
2021-12-21 04:01:33 +0100

    7.5 Functionally calculating the average task

---
+++ Kodilla-Stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.OptionalDouble;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+
+        //Then
+        assertEquals(5.5, average);
+    }
+
+}
