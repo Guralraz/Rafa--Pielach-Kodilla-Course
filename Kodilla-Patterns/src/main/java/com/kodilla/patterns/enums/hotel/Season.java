7ae0286021e9f66e54a82d0c2ad765376fe22846
Guralraz <87212802+Guralraz@users.noreply.github.com>
2022-02-16 17:51:13 +0100

    15.1

---
+++ Kodilla-Patterns/src/main/java/com/kodilla/patterns/enums/hotel/Season.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns.enums.hotel;
+
+import java.math.BigDecimal;
+
+public enum Season {
+    LOW(new BigDecimal(250), new BigDecimal(350)),
+    HIGH(new BigDecimal(320), new BigDecimal(400)),
+    HOLIDAY(new BigDecimal(400), new BigDecimal(500));
+
+    private BigDecimal singleBedRoomPrice;
+
+    private BigDecimal doubleBedRoomPrice;
+
+    Season(BigDecimal singleBedRoomPrice, BigDecimal doubleBedRoomPrice) {
+        this.singleBedRoomPrice = singleBedRoomPrice;
+        this.doubleBedRoomPrice = doubleBedRoomPrice;
+    }
+
+}
